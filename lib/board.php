e234b64504c69d44a7bc30615d301a008d33e62e
Igneez <mixalisol@hotmail.com>
2021-01-02 12:34:26 +0200

    changes

--- lib/board.php
+++ lib/board.php
@@ -1,26 +1,34 @@
 
 <?php
-function show_board($input) {
+function show_board() {
 	global $mysqli;
 	
-	$b=current_color($input['token']);
+	$sql = 'select * from board';
+	$st = $mysqli->prepare($sql);
+	
+	$st->execute();
+	$res = $st->get_result();
+	
+/*	$b=current_color($input['token']);
 	if($b) {
 		show_board_by_player($b);
 	} else {
+		*/
 		header('Content-type: application/json');
-		print json_encode(read_board(), JSON_PRETTY_PRINT);
-	}
-}
+		print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+	
+//}}
 
 function reset_board() {
 	global $mysqli;
 	$sql = 'call clean_board()';
 	$mysqli->query($sql);
+	show_board();
 	
 }
 
 
 
 
-
+}
 ?>
